1be28543ea6ba4e84fbcb0131d8ff760f61e538f
Evg <zhekavafiev#gmail.com>
2020-04-19 22:55:53 +0500

    Add test for json format, correct mistakes, add test coverage

--- tests/differTest.php
+++ tests/differTest.php
@@ -8,21 +8,41 @@ use function Differ\Differ\genDiff;
 
 class DiffTest extends TestCase
 {
-    public function testDiff()
+    public function testDiffFlat()
     {
-        $expected1 = file_get_contents("tests/fixtures/datafortestflat");
-        $expected2 = file_get_contents("tests/fixtures/datafortestflat");
-        $expected3 = file_get_contents("tests/fixtures/datafortestnested");
-        $expected4 = file_get_contents("tests/fixtures/datafortestplain");
+        $expected = file_get_contents("tests/fixtures/datafortestflat");
 
         $actual1 = genDiff("tests/fixtures/beforeflat.json", "tests/fixtures/afterflat.json");
         $actual2 = genDiff("tests/fixtures/beforeflat.yml", "tests/fixtures/afterflat.yml");
-        $actual3 = genDiff("tests/fixtures/beforenested.json", "tests/fixtures/afternested.json");
-        $actual4 = genDiff("tests/fixtures/beforenested.json", "tests/fixtures/afternested.json", 'plain');
         
-        $this->assertEquals($expected1, $actual1);
-        $this->assertEquals($expected2, $actual2);
-        $this->assertEquals($expected3, $actual3);
-        $this->assertEquals($expected4, $actual4);
+        $this->assertEquals($expected, $actual1);
+        $this->assertEquals($expected, $actual2);
+    }
+
+    public function testDiffNested()
+    {
+        $expected = file_get_contents("tests/fixtures/datafortestnested");
+
+        $actual = genDiff("tests/fixtures/beforenested.json", "tests/fixtures/afternested.json");
+        
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testDiffPlain()
+    {
+
+        $expected = file_get_contents("tests/fixtures/datafortestplain");
+        $actual = genDiff("tests/fixtures/beforenested.json", "tests/fixtures/afternested.json", 'plain');
+        
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testDiffJson()
+    {
+
+        $expected = file_get_contents("tests/fixtures/datafortestjson");
+        $actual = genDiff("tests/fixtures/beforenested.json", "tests/fixtures/afternested.json", 'json');
+        
+        $this->assertEquals($expected, $actual);
     }
 }
