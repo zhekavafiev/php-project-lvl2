d64c9862bdbecb5433fb8cccc6610283f64bd011
Evg <zhekavafiev#gmail.com>
2020-05-09 18:19:14 +0500

    Delete if-construction Add iter-function

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -4,39 +4,49 @@ namespace Differ\Formatters\Plain;
 
 function render($tree)
 {
-    print_r($tree);
-    $acc = "";
-    $res = function ($node, &$acc, $road = "") use (&$res) {
-        $abc = array_reduce($node, function ($acc, $el) use (&$res, $road) {
-            if ($el['type'] == 'Added') {
-                $road .= "{$el['name']}";
-                if (!is_object($el['newValue'])) {
+    return iter($tree);
+}
+
+function iter($tree, $acc = '', $roadToProperties = '')
+{
+    $putDescription = array_reduce($tree, function ($acc, $node) use ($roadToProperties) {
+        $type = $node['type'];
+        $name = $node['name'];
+        
+        switch ($type) {
+            case 'Added':
+                $roadToProperties .= "{$name}";
+                if (!is_object($node['newValue'])) {
                     $acc .=
-                    "Property '{$road}' was added with value: '{$el['newValue']}'\n";
+                    "Property '{$roadToProperties}' was added with value: '{$node['newValue']}'\n";
                 } else {
                     $acc .=
-                    "Property '{$road}' was added with value: 'complex value'\n";
+                    "Property '{$roadToProperties}' was added with value: 'complex value'\n";
                 }
-            }
-
-            if ($el['type'] == 'Removed') {
-                $road .= "{$el['name']}";
-                $acc .= "Property '{$road}' was removed\n";
-            }
-
-            if ($el['type'] == 'Changed') {
-                $road .= "{$el['name']}";
+                break;
+            
+            case 'Unchanged':
+                break;
+            
+            case 'Removed':
+                $roadToProperties .= "{$name}";
+                $acc .= "Property '{$roadToProperties}' was removed\n";
+                break;
+            
+            case 'Changed':
+                $roadToProperties .= "{$name}";
                 $acc .=
-                "Property '{$road}' was changed. From '{$el['oldValue']}' to '{$el['newValue']}'\n";
-            }
-
-            if ($el['type'] == 'Nested') {
-                $road .= "{$el['name']}.";
-                return $res($el['children'], $acc, $road);
-            }
-            return $acc;
-        }, $acc);
-        return $abc;
-    };
-    return $res($tree, $acc);
+                "Property '{$roadToProperties}' was changed. From '{$node['oldValue']}' to '{$node['newValue']}'\n";
+                break;
+            
+            case 'Nested':
+                $roadToProperties .= "{$name}.";
+                return iter($node['children'], $acc, $roadToProperties);
+            default:
+                break;
+        }
+        return $acc;
+    }, $acc);
+    
+    return $putDescription;
 }
