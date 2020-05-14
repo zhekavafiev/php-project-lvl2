11dde9f9f6149aab1250f294c1ead4d074474c9b
Evg <zhekavafiev#gmail.com>
2020-05-14 16:35:30 +0500

    Add filtration on unchenged elements

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -2,13 +2,19 @@
 
 namespace Differ\Formatters\Plain;
 
+use function Funct\Collection\compact;
+
 function render($tree)
 {
-    return implode('', iter($tree));
+    return implode("\n", (iter($tree)));
 }
 
 function iter($tree, $ancestry = '')
 {
+    $filtered = array_filter($tree, function ($node) {
+        return $node['type'] != 'Unchanged';
+    });
+
     $mapping = array_map(function ($node) use ($ancestry) {
         $newAncestry = $ancestry . $node['name'];
         $type = $node['type'];
@@ -16,29 +22,26 @@ function iter($tree, $ancestry = '')
         switch ($type) {
             case 'Added':
                 $value = stringify($node['newValue']);
-                return "Property '{$newAncestry}' was added with value: '{$value}'\n";
-            
-            case 'Unchanged':
-                break;
+                $formatView = "Property '%s' was added with value: '%s'";
+                return sprintf($formatView, $newAncestry, $value);
             
             case 'Removed':
-                return "Property '{$newAncestry}' was removed\n";
+                return "Property '{$newAncestry}' was removed";
             
             case 'Changed':
                 $newVal = stringify($node['newValue']);
                 $oldval = stringify($node['oldValue']);
-                return
-                "Property '{$newAncestry}' was changed. From '{$oldval}' to '{$newVal}'\n";
-                break;
-            
+                $formatView = "Property '%s' was changed. From '%s' to '%s'";
+                return sprintf($formatView, $newAncestry, $oldval, $newVal);
+                
             case 'Nested':
                 $newAncestry .= '.';
-                $stepOnDepth = iter($node['children'], $newAncestry);
-                return implode('', $stepOnDepth);
+                $result = (iter($node['children'], $newAncestry));
+                return implode("\n", $result);
             default:
                 break;
         }
-    }, $tree);
+    }, $filtered);
     
     return $mapping;
 }
