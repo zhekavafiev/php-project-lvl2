3814601c89afda8329ffc86002835b0f4ccc135d
Evg <zhekavafiev#gmail.com>
2020-05-19 17:11:30 +0500

    Remode normalizePath function

--- src/Differ.php
+++ src/Differ.php
@@ -8,23 +8,18 @@ use function Differ\Formatters\render;
 
 function genDiff($path1, $path2, $format = 'pretty')
 {
-    $normalizedPath1 = normalizePath($path1);
+    $normalizedPath1 = realpath($path1);
     $rawData1 = file_get_contents($normalizedPath1);
     $dataType1 = getTypeFile($path1);
     $parsedData1 = parse($rawData1, $dataType1);
     
-    $normalizedPath2 = normalizePath($path2);
+    $normalizedPath2 = realpath($path2);
     $rawData2 = file_get_contents($normalizedPath2);
     $dataType2 = getTypeFile($path2);
     $parsedData2 = parse($rawData2, $dataType2);
 
     $internalTree = buildAst($parsedData1, $parsedData2);
     return render($internalTree, $format);
-}
-
-function normalizePath($path)
-{
-    return realpath($path);
 }
 
 function getTypeFile($path)
